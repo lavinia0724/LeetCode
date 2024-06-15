247632705d278790446dbc00903fa0d269b33d05
unknown <LAVI@nisra.net>
2024-06-15 23:24:01 +0800

    add Heap/502. IPO.py

--- Heap/502. IPO.py
+++ Heap/502. IPO.py
@@ -8,16 +8,16 @@ class Solution(object):
         :type capital: List[int]
         :rtype: int
         """
-        # 可以一口氣放兩個組，且會使用第一組來排序
-        project = [(capital[i], profits[i]) for i in range(len(profits))]
-        project.sort()
+        # 可以一口氣放兩個組，使用 capital 來排序
+        projects = [(capital[i], profits[i]) for i in range(len(profits))]
+        projects.sort()
 
         maxHeap = []
         i = 0
         for _ in range(k):
-            while(i < len(project) and project[i][0] <= w):
-                    # 因為 heapq 由小到大排序，所以 push -project[i][1]，這樣最大的反而最小，等等 w 再用 - 的讓他變回正數
-                    heapq.heappush(maxHeap, -project[i][1])
+            while(i < len(projects) and projects[i][0] <= w):
+                    # 因為 heapq 由小到大排序，所以 push -projects[i][1]，這樣最大的反而最小，等等 w 再用 - 的讓他變回正數
+                    heapq.heappush(maxHeap, -projects[i][1])
                     i += 1
             # if not <list name> 可以確認 list 是否 empty
             if not maxHeap:
