fb8f2acf69555842eef685ded6c43c4ac65041f6
unknown <LAVI@nisra.net>
2025-11-29 22:00:57 +0800

    add Array/3512._Minimum_Operations_to_Make_Array_Sum_Divisible_by_K.cpp

---
+++ Array/3512. Minimum Operations to Make Array Sum Divisible by K.cpp
@@ -0,0 +1,8 @@
+// 2025/11/29
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        return (sum >= k) ? sum % k : sum;
+    }
+};
